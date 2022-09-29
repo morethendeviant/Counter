b7163701c2959f067c0577281aaa64438da422cf
Aleksandr Velikanov <velikanov.atwork@gmail.com>
2022-09-29 23:11:31 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Aleksandr Velikanov on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
